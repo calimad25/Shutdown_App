39136aa3cd525271722d814bb4e2bcc970969c78
calimad <calimanescuadrian@gmail.com>
2023-04-23 10:45:18 +0300

    Creating the methods and the basic GUI

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import tkinter as tk
 import os
 
 
-def shutdown():
-    pass
+def shutdown(time):
+    """Method that will shutdown the computer after a desired ammount of time"""
+    return os.system(f"shutdown -s -t {time}")
 
 
 def cancel_shutdown():
-    pass
+    """Stops the active shutdown log"""
+    return os.system("shutdown -a")
 
 
 class Shutdown:
 
     def __init__(self):
 
-        pass
+        self.root =tk.Tk()
+        self.root.geometry("300x300")
+        self.root.title("Shutdown App")
 
+        self.root.mainloop()
 
 screen = Shutdown()
