6379f56833836088fb1fdf143b24ec4c121d3d5e
calimad <calimanescuadrian@gmail.com>
2023-04-23 10:53:45 +0300

    Adding functionality to the app

--- main.py
+++ main.py
@@ -20,6 +20,29 @@ class Shutdown:
         self.root.geometry("300x300")
         self.root.title("Shutdown App")
 
+        # Buttons with various predifined ammount of time
+        self.button1 = tk.Button(self.root, text="30 min", command=lambda: shutdown(1800))
+        self.button1.pack(pady=10)
+
+        self.button2 = tk.Button(self.root, text="1 hour", command=lambda: shutdown(3600))
+        self.button2.pack(pady=10)
+
+        self.button3 = tk.Button(self.root, text="2 hours", command=lambda: shutdown(7200))
+        self.button3.pack(pady=10)
+
+        self.button4 = tk.Button(self.root, text="4 hours", command=lambda: shutdown(14400))
+        self.button4.pack(pady=10)
+
+        # An input field for the user to specify any ammount of time. Also a custom button
+        self.custom_widget = tk.Entry(width=10)
+        self.custom_widget.pack()
+        self.button5 = tk.Button(self.root, text="Custom", command=lambda: shutdown(self.custom_widget.get()))
+        self.button5.pack(pady=10)
+
+        # The cancel button
+        self.button6 = tk.Button(self.root, text="Cancel Shutdown", command=cancel_shutdown)
+        self.button6.pack(pady=10)
+
         self.root.mainloop()
 
 screen = Shutdown()
