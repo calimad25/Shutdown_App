d554962f50de56b2d97200d664f5b0e32dbdd90c
calimad <calimanescuadrian@gmail.com>
2023-04-23 10:38:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import os
+
+
+def shutdown():
+    pass
+
+
+def cancel_shutdown():
+    pass
+
+
+class Shutdown:
+
+    def __init__(self):
+
+        pass
+
+
+screen = Shutdown()
